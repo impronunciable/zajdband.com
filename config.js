76e49c258146e67d0507f117d248d05597f8a198
Dan Zajdband <dan@accelrobotics.com>
2019-11-10 17:37:26 -0300

    config

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://zajdband.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Dan Zajdband',
+  subtitle: 'JavaScript mutant ninja turtle',
   copyright: '',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-29730613-1',
   useKatex: false,
   menu: [
     {
